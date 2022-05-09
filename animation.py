7999b0225930477e36c92f043ecdb6a1dff04b2b
radRoy <daniel.walther@uzh.ch>
2022-05-09 14:08:57 +0200

    Update animation.py

--- animation.py
+++ animation.py
@@ -6,46 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-class Simulation:
-    def __init__(self):
-        self.actors = []  # the list to animate for each frame (see Simulation.run())
-
-    def setup(self, nboids):
-        # Create random positions and velocities
-        positions = np.random.rand(nboids, 2)
-        velocities = np.random.rand(nboids, 2)
-
-        flock = []
-
-        # Populate the actors with new boids
-        for i in range(nboids):
-            new = Boid(position=positions[i],
-                       velocity=velocities[i],
-                       speed=1,
-                       view_distance=2,
-                       view_angle=2 * np.pi,
-                       flock=flock)
-
-            # QU: Is new.flock an alias of flock?
-            # (= will new.flock be updated when another Boid() joins the flock?)
-            new.flock = flock
-            flock.append(new)
-            self.actors.append(new)
-
-    def run(self, steps, dt):
-        # ani: t=0
-        for i in range(steps):  # ani: change to while True loop? break if window closed?
-            self.step(dt)
-            # ani: t+=dt
-
-    def step(self, dt):
-        for actor in self.actors:
-            actor.move(dt)  # currently, velocity is constant
-            # needed for animation: actor.pos, actor.v
-
-
 def animate(i, ani, dt_animate):
-    ani: Simulation
     boids = ani.actors
 
     plt.clf()  # clear axis, if axis should change
