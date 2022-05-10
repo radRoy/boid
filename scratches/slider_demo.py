feb225ac418bc369cd13f8a2b22861c3961ab3a0
radRoy <daniel.walther@uzh.ch>
2022-05-10 22:36:06 +0200

    slider in still plot works in VS code - TBD: merge slider with live animation

--- scratches/slider_demo.py
+++ scratches/slider_demo.py
@@ -47,7 +47,7 @@ plt.subplots_adjust(left=0.25, bottom=0.25)
 axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
 freq_slider = Slider(
     ax=axfreq,
-    label='Frequency [Hz]',
+    label='Frequency, YAY IT WORKS [Hz]',
     valmin=0.1,
     valmax=30,
     valinit=init_frequency,
