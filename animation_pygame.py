28ade038c662c66f2a22901eba7d5e39b491113e
radRoy <daniel.walther@uzh.ch>
2022-05-14 22:44:44 +0200

    Update animation_pygame.py, go bed now

--- animation_pygame.py
+++ animation_pygame.py
@@ -1,5 +1,8 @@
 """animation_pygame.py
-code adapted from https://www.pygame.org/docs/tut/PygameIntro.html and https://www.pygame.org/docs/ in general"""
+code adapted from https://www.pygame.org/docs/tut/PygameIntro.html and https://www.pygame.org/docs/ in general
+images taken from:
+    - https://www.iconfinder.com/icons/1051165/arrow_head_arrowhead_direction_move_orientation_pointer_right_icon (rightward arrowhead)
+    - https://www.iconfinder.com/icons/1051162/arrow_head_arrowhead_direction_move_orientation_pointer_send_up_icon (upward arrowhead)"""
 
 import sys
 import pygame
@@ -11,13 +14,14 @@ def main(sim: s.Simulation, fps: int) -> None:
 
     pygame.init()
     size = (width, height) = 1028, 720  # pygame window dimensions
-    speed = sim.actors.speeds  # TBD
-    black = 0, 0, 0
-
     screen = pygame.display.set_mode(size)
 
+    black = 0, 0, 0
     boid = pygame.image.load("arrowhead_upwards.png")
     boid_rect = boid.get_rect()
+
+    boid_speed = sim.actors[0].v
+    boid_position = sim.actors[0].pos
 
     while True:
 
@@ -27,11 +31,16 @@ def main(sim: s.Simulation, fps: int) -> None:
                 sys.exit()
 
         # UPDATE THE FRAME
-        boid_rect
+        boid_rect = boid_rect.move
 
 
 if __name__ == "__main__":
     sim_test = s.Simulation()
     sim_test.setup(nboids=1)
 
-    main(sim=sim_test, fps=30)
+    print(f"sim_test.actors: {sim_test.actors}")
+    print("\n")
+    for x in sim_test.actors:
+        print(x)
+
+    #main(sim=sim_test, fps=30)
