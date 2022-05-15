ca8600666b760be2f5006787d3428bc324a760be
radRoy <daniel.walther@uzh.ch>
2022-05-15 19:25:12 +0200

    cleaned code, recording doesn't work atm (fraps ez fix?)

--- animation_pygame.py
+++ animation_pygame.py
@@ -1,4 +1,5 @@
 """animation_pygame.py
+in pygame, every image is a pygame.surface.Surface object, including the displayed image surface
 code adapted from https://www.pygame.org/docs/tut/PygameIntro.html and https://www.pygame.org/docs/ in general.
 images taken from:
     - https://www.iconfinder.com/icons/1051165/arrow_head_arrowhead_direction_move_orientation_pointer_right_icon (rightward arrowhead)
@@ -7,61 +8,65 @@ abbreviations used:
     - RT: real time"""
 
 import sys
-import pygame
+import pygame as pg
+import pygame.camera  # CAUTION: code will very likely break with next pygame release! (see doc pg.camera.Camera)
 import simulation as s
 
 
 def main(sim: s.Simulation, fps: int) -> None:
     """the function in charge of handling all function calls regarding the live animation interface and display"""
     dt = fps / 1000
-    # boid = sim.actors[0]
-    boids = sim.actors
+    actors = sim.actors
     rects = []
-    arrows = []
 
-    pygame.init()
-    size = (res_x, res_y) = 1280, 720
-    screen = pygame.display.set_mode(size)
-    black = 0, 0, 0
+    pg.init()  # multiple .init()s don't hurt pygame
+    # pg.camera.init()
+    # pg.camera.init('_camera (MSMF)')
+    # neither cameras make ..Camera.start() work (not found)
+    # pg.camera.Camera.start()  # not found
+
+    size = (res_x, res_y) = 720, 720
+    screen = pg.display.set_mode(size)
+    # black = 0, 0, 0
     white = 255, 255, 255
     background = white
+    # TBD: add a grid for position tracking in moving screen
 
     # INIT RECTS POSITIONS (& init frame, not required for ani. to work)
-    arrow = pygame.image.load("arrowhead_upwards_tiny.png")
-    screen.fill(background)  # add a grid for position tracking in moving screen
-    # for i, boid in enumerate(boids):
-    for i, boid in enumerate(sim.actors):
+    arrow = pg.image.load("arrowhead_upwards_tiny.png")
+    screen.fill(background)
+    for i, boid in enumerate(actors):
         rect = arrow.get_rect()
         rect = rect.move(boid.pos)
         rects.append(rect)
-        # rects.append(arrow.get_rect())
-        # rects[i] = rects[i].move(boid.pos)
-        # screen.blit(arrow, rects[i])
-        screen.blit(arrow, rect)
-    pygame.display.flip()
+        screen.blit(arrow, rect)  # look into .blits() for one-liner of multi-Surface animation
+    pg.display.update()  # more efficient & capable version of .display.flip() (flip() must do vsync, update() can)
 
     while True:
-        for event in pygame.event.get():
-            # print(f"event = {event}")
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            # print(f"event = {event}")  # registers keyboard & mouse inputs & cursor location with or without usage
+            if event.type == pg.QUIT:
                 sys.exit()
 
         # UPDATE Simulation()
         sim.step(dt)  # = sim.run(steps=1, dt=dt)
 
         # UPDATE DISPLAY
-        screen.fill(background)  # add a grid for position tracking in moving screen
+        screen.fill(background)  # comment out to see movement trajectories
         for i, rect in enumerate(rects):
-            # rect = rect.move(boids[i].pos)
-            # rect = rect.move([sim.actors[i].pos[0]*20, sim.actors[i].pos[1]*20])
-            rect = rect.move([boids[i].pos[0]*50, boids[i].pos[1]*50])
-            # screen.blit(arrows[i], rect)
+            boid = actors[i]
+            # rect = rect.move(boid.pos)  # boids overlap, currently
+            rect = rect.move([boid.pos[0]*50, boid.pos[1]*50])  # boids don't overlap
             screen.blit(arrow, rect)
-        pygame.display.flip()
+        pg.display.update()
+
+    # pg.camera.Camera.stop()  # .start() not found
 
 
 if __name__ == "__main__":
     sim_test = s.Simulation()
-    sim_test.setup(nboids=50)
+    sim_test.setup(nboids=30)
 
-    main(sim=sim_test, fps=60)
+    # pg.init()
+    # print(pg.camera.get_backends())  # MSMF seems like earliest implemented Camera in pygame
+    main(sim=sim_test, fps=30)
