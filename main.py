287e329d475607fdf787c72314679330701ae34b
Jacqueline Ulken <jacqueline.ulken@uzh.ch>
2022-05-26 11:28:24 +0200

    copied slider code into main.py

--- main.py
+++ main.py
@@ -16,6 +16,74 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
+def setup_sliders(display):
+    fontsize = 28
+
+    # Slider settings
+    s_left = 15  # distance to left window border (x-dir.) in px
+    s_top = 15  # distance to top window border (x-dir.) in px
+    s_width = 150  # width (x-dir.) in px
+    s_height = 10  # height (y-dir.) in px
+    s_dist = 50  # distance between sliders (y-dir.) in px
+
+    # TextBox settings
+    t_dist = 20  # dist. to corr. slider (above in animation)
+    t_left = s_left + s_width + 5  # dist. to left border
+    t_top = s_top + t_dist
+    t_width, t_height = 30, 20
+
+    # Sliders and TextBoxes by Simulation parameter
+    sep_slider = Slider(display, s_left, s_top, s_width, s_height, min=0.0, max=10.0, step=.1)
+    sep_out = TextBox(display, t_left, t_top, 0, t_height, fontSize=fontsize, borderThickness=1)
+    sep_label = TextBox(display, s_left, s_top + t_dist, 0, t_height, fontSize=fontsize, borderThickness=1).setText(
+        "separation")
+    coh_slider = Slider(display, s_left, s_top + s_dist, s_width, s_height, min=0.0, max=10.0, step=.1)
+    coh_out = TextBox(display, t_left, t_top + s_dist, 0, t_height, fontSize=fontsize, borderThickness=1)
+    coh_label = TextBox(display, s_left, s_top + s_dist + t_dist, 0, t_height, fontSize=fontsize,
+                        borderThickness=1).setText("coherence")
+    align_slider = Slider(display, s_left, s_top + s_dist * 2, s_width, s_height, min=0.0, max=10.0, step=.1)
+    align_out = TextBox(display, t_left, t_top + s_dist * 2, 0, t_height, fontSize=fontsize, borderThickness=1)
+    align_label = TextBox(display, s_left, t_top + s_dist * 2, 0, t_height, fontSize=fontsize,
+                          borderThickness=1).setText("alignment")
+    maxvel_slider = Slider(display, s_left, s_top + s_dist * 4, s_width, s_height, min=0.0, max=10.0, step=.1)
+    maxvel_out = TextBox(display, t_left, t_top + s_dist * 4, 0, t_height, fontSize=fontsize, borderThickness=1)
+    maxvel_label = TextBox(display, s_left, t_top + s_dist * 4, 0, t_height, fontSize=fontsize,
+                           borderThickness=1).setText("slider 4")
+    slider_5 = Slider(display, s_left, s_top + s_dist * 5, s_width, s_height, min=0.0, max=10.0, step=.1)
+    slider_5_out = TextBox(display, t_left, t_top + s_dist * 5, 0, t_height, fontSize=fontsize, borderThickness=1)
+    slider_5_label = TextBox(display, s_left, t_top + s_dist * 5, 0, t_height, fontSize=fontsize,
+                             borderThickness=1).setText("slider 5")
+    # slider_6 = Slider(display, s_left, s_top + s_dist * 6, s_width, s_height, min=0.0, max=10.0, step=.1)
+    # slider_6_out = TextBox(display, t_left, t_top + s_dist * 6, 0, t_height, fontSize=fontsize, borderThickness=1)
+    # slider_6_label = TextBox(display, s_left, t_top + s_dist * 6, 0, t_height, fontSize=fontsize, borderThickness=1).setText("slider 6")
+    # slider_7 = Slider(display, s_left, s_top + s_dist * 7, s_width, s_height, min=0.0, max=10.0, step=.1)
+    # slider_7_out = TextBox(display, t_left, t_top + s_dist * 7, 0, t_height, fontSize=fontsize, borderThickness=1)
+    # slider_7_label = TextBox(display, s_left, t_top + s_dist * 7, 0, t_height, fontSize=fontsize, borderThickness=1).setText("slider 7")
+    # slider_8 = Slider(display, s_left, s_top + s_dist * 8, s_width, s_height, min=0.0, max=10.0, step=.1)
+    # slider_8_out = TextBox(display, t_left, t_top + s_dist * 8, 0, t_height, fontSize=fontsize, borderThickness=1)
+    # slider_8_label = TextBox(display, s_left, t_top + s_dist * 8, 0, t_height, fontSize=fontsize, borderThickness=1).setText("slider 8")
+    # slider_9 = Slider(display, s_left, s_top + s_dist * 9, s_width, s_height, min=0.0, max=10.0, step=.1)
+    # slider_9_out = TextBox(display, t_left, t_top + s_dist * 9, 0, t_height, fontSize=fontsize, borderThickness=1)
+    # slider_9_label = TextBox(display, s_left, t_top + s_dist * 9, 0, t_height, fontSize=fontsize, borderThickness=1).setText("slider 9")
+
+    slider_settings = {"sep_slider": sep_slider, "coh_slider": coh_slider, "align_slider": align_slider,
+                       "sep_out": sep_out, "coh_out": coh_out, "align_out": align_out}
+
+    return slider_settings
+
+
+def slider_update(slider_settings):
+    # feeding slider values into simulation parameters
+    actors.separation_strength = slider_settings["sep_slider"].getValue()  # 7.0 (yep, dem is birds, alright)
+    actors.cohesion_strength = slider_settings["coh_slider"].getValue()  # 3.0
+    actors.alignment_strength = slider_settings["align_slider"].getValue()  # 3.5
+
+    # each following drawing command draws on top of previous ones (so, leave widgets.update on top)
+    slider_settings["sep_out"].setText(actors.separation_strength)
+    slider_settings["coh_out"].setText(actors.cohesion_strength)
+    slider_settings["align_out"].setText(actors.alignment_strength)
+
+
 def draw_actors(sim, display):
     """Iterates over all the actors in the simulation and draws them on screen."""
     for actor in sim.actors:
@@ -51,6 +119,8 @@ def main(sim, fps, window_size):
     display = pg.display.set_mode(window_size)
     clock = pg.time.Clock()
 
+    # slider_settings = setup_sliders(display)
+
     mouse_circle = None
 
     # Main game loop
@@ -68,7 +138,10 @@ def main(sim, fps, window_size):
                 sim.add_obstacles(mouse_circle)
 
         display.fill(WHITE)
+        pg_widgets.update(events)
         dt = clock.tick(fps)
+
+        # slider_update(slider_settings)
 
         sim.step(dt)
         draw_actors(sim, display)
